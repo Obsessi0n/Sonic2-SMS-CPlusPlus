6097d7c6332b7c567913a528d3d7dce6aabfd7ff
Cascavel <simao.ramaral@gmail.com>
2021-09-14 10:50:57 +0100

    Possible fix for multiple keypresses

--- InputManager.cpp
+++ InputManager.cpp
@@ -9,21 +9,21 @@ void Sanic::InputManager::HandleInput()
 	SDL_Event event;
 	const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
 
-	if (SDL_PollEvent(&event))
+	while (SDL_PollEvent(&event))
 	{
 		if (event.type == SDL_QUIT)
 			_Game::Instance()->quit();
+	}
 
-		if (currentKeyStates[SDL_SCANCODE_UP]) {
-			arrowUp_->execute();
-		}
+	if (currentKeyStates[SDL_SCANCODE_UP]) {
+		arrowUp_->execute();
+	}
 
-		if (currentKeyStates[SDL_SCANCODE_LEFT]) {
-			arrowLeft_->execute();
-		}
+	if (currentKeyStates[SDL_SCANCODE_LEFT]) {
+		arrowLeft_->execute();
+	}
 
-		if (currentKeyStates[SDL_SCANCODE_RIGHT]) {
-			arrowRight_->execute();
-		}
+	if (currentKeyStates[SDL_SCANCODE_RIGHT]) {
+		arrowRight_->execute();
 	}
 }
