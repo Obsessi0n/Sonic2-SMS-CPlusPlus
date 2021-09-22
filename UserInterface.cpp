5693fd569d7e37c30e8404d063990010b0c8d4c4
Cascavel <simao.ramaral@gmail.com>
2021-09-22 11:09:49 +0100

    timer UI added (non functional)

--- UserInterface.cpp
+++ UserInterface.cpp
@@ -1,10 +1,12 @@
 #include "UserInterface.h"
 #include "TextureManager.h"
 #include "Game.h"
+#include <chrono>
 
 Sanic::UserInterface::UserInterface()
 {
 	Sanic::_TextureManager::Instance()->Load("assets/sprites/player/ui.png", "interface", Sanic::_Game::Instance()->getRenderer());
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 }
 
 void Sanic::UserInterface::TopUI()
@@ -17,6 +19,14 @@ void Sanic::UserInterface::TopUI()
 
 	Sanic::_TextureManager::Instance()->DrawFrame("interface", (tX + offset) + m_width, tY, m_width, m_height, 1, 0, 2, Sanic::_Game::Instance()->getRenderer());
 	Sanic::_TextureManager::Instance()->DrawFrame("interface", (tX + offset) + (m_width * 2) - offset, tY, m_width, m_height, 1, 0, 2, Sanic::_Game::Instance()->getRenderer());
+
+	//Timer
+	Sanic::_TextureManager::Instance()->DrawFrame("interface", tX + offset, tY + m_height, m_width, m_height, 1, 0, 2, Sanic::_Game::Instance()->getRenderer());
+
+	Sanic::_TextureManager::Instance()->DrawFrame("interface", (tX + m_width) + 2, (tY + m_height) - 1, m_width, m_height, 1, 1, 1, Sanic::_Game::Instance()->getRenderer());
+
+	Sanic::_TextureManager::Instance()->DrawFrame("interface", (tX + offset) + m_width, tY + m_height, m_width, m_height, 1, 0, 2, Sanic::_Game::Instance()->getRenderer());
+	Sanic::_TextureManager::Instance()->DrawFrame("interface", (tX + offset) + (m_width + offset), tY + m_height, m_width, m_height, 1, 0, 2, Sanic::_Game::Instance()->getRenderer());
 
 }
 
